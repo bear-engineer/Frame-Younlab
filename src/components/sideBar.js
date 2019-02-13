9f76583ace6554b21acbd039927b1486ef23895e
Sehyeon Youn <dev.younlab@gmail.com>
2019-02-14 02:20:17 +0900

    [UPDATE] sideBar end

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -1,10 +1,29 @@
 import React from "react";
 import kebabCase from "lodash/kebabCase";
-
+import "./styles/sideBar.scss";
 export default ({ data }) => (
-  <div>
-    <ul>
-      <h2>Tags</h2>
+  <nav className="side-bar">
+    <h1 className="side-bar-title">
+      Younlab <span>Frame</span>
+    </h1>
+    <form
+      action=""
+      className="side-bar-search"
+      onSubmit={e => e.preventDefault()}
+    >
+      <input type="text" placeholder="Search Document" />
+    </form>
+    <ul className="side-bar-menu">
+      <h2 className="side-bar-menu-title">Menu</h2>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/about">About</a>
+      </li>
+    </ul>
+    <ul className="side-bar-menu side-bar-menu-tag">
+      <h2 className="side-bar-menu-title">Tags</h2>
       {data.map(tag => (
         <li key={tag.fieldValue}>
           <a href={`tags/${kebabCase(tag.fieldValue)}`}>
@@ -14,5 +33,5 @@ export default ({ data }) => (
         </li>
       ))}
     </ul>
-  </div>
+  </nav>
 );
