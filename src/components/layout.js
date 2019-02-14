3e5cfd57321bd70022e1e7c894f6b40ef0ea4ffd
Sehyeon Youn <dev.younlab@gmail.com>
2019-02-15 00:41:09 +0900

    [ADD] menu toggle function add

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,14 @@ import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
 import SideBar from "./container/sideBar";
 import "./styles/layout.scss";
-
+const menu = e => {
+  const leftSideBar = document.querySelector(".layout-left-side-wrap");
+  const rightSideContent = document.querySelector(".layout-right-side-wrap");
+  const menuBtnArrow = document.querySelector(".layout-menu-btn-arrow");
+  leftSideBar.classList.toggle("left-active");
+  rightSideContent.classList.toggle("right-active");
+  menuBtnArrow.classList.toggle("arrow-active");
+};
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
@@ -35,6 +42,9 @@ const Layout = ({ children }) => (
           </aside>
         </section>
         <section className="layout-right-side-wrap">
+          <div className="layout-menu-btn" onClick={e => menu(e)}>
+            <div className="layout-menu-btn-arrow">▶</div>
+          </div>
           <main className="container layout-content-wrap">{children}</main>
         </section>
       </div>
