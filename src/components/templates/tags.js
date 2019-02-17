34e2ecb04ecc31108ac26a25fa30865987e70687
Sehyeon Youn <dev.younlab@gmail.com>
2019-02-17 19:50:38 +0900

    [UPDATE] site metadata title, helmet setting

--- src/components/templates/tags.js
+++ src/components/templates/tags.js
@@ -2,11 +2,19 @@ import { graphql } from "gatsby";
 import React, { Component } from "react";
 import Layout from "../layout";
 import Post from "../container/post";
+import { Helmet } from "react-helmet";
 class Tag extends Component {
   render() {
     const data = this.props.data.allMarkdownRemark.edges;
     return (
       <Layout>
+        {console.log(this.props.pageContext.tag)}
+        <Helmet>
+          <title>
+            {this.props.data.site.siteMetadata.title} | Tag:{" "}
+            {this.props.pageContext.tag}
+          </title>
+        </Helmet>
         {data.map(tag => (
           <Post
             title={tag.node.frontmatter.title}
@@ -27,6 +35,11 @@ export default Tag;
 
 export const query = graphql`
   query($tag: String) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     allMarkdownRemark(
       limit: 2000
       sort: { fields: [frontmatter___date], order: DESC }
